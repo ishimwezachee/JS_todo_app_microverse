bab0929c372e4c05eac78e2c7845a5165f23262e
ishimwezachee <zacheeishimwe@gmail.com>
2021-10-26 12:51:23 +0200

    Add static version of todo list with html and css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-      // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+
+const tasks = [
+  {
+    index:0,
+    describtion:'Wake up',
+    completed:false,
+  },
+  {
+    index:1,
+    describtion:'Go to shower',
+    completed:false,
+  },
+  {
+    index:2,
+    describtion:'Take breakfast',
+    completed:false,
+  },
+  {
+    index:3,
+    describtion:'start class',
+    completed:false,
+  },
+]
