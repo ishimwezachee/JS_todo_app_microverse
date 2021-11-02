05fc49ad68b693b1672f8224bf81fe4d9de233c3
ishimwezachee <zacheeishimwe@gmail.com>
2021-11-02 16:01:12 +0200

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,65 +1,58 @@
-import _ from 'lodash';
-import "./style.css";
+import './style.css';
 
 const tasksArr = [
   {
     index: 0,
-    describtion: "Wake up",
+    describtion: 'Wake up',
     completed: false,
   },
   {
     index: 1,
-    describtion: "Go to shower",
+    describtion: 'Go to shower',
     completed: false,
   },
   {
     index: 2,
-    describtion: "Take breakfast",
+    describtion: 'Take breakfast',
     completed: false,
   },
   {
     index: 3,
-    describtion: "start class",
+    describtion: 'start class',
     completed: false,
   },
 ];
 
 // selectors
-
-const todoInput = document.querySelector(".add");
-const addTodoBtn = document.querySelector(".arrow");
-const list = document.querySelector(".list");
+const list = document.querySelector('.list');
 
 // Methods/functions
 const addTodo = (value) => {
-  const listItem = document.createElement("li");
-  listItem.classList.add("list-item");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-item');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
   checkbox.classList.add('check');
-  const para = document.createElement("p");
-  para.classList.add("task");
+  const para = document.createElement('p');
+  para.classList.add('task');
   para.textContent = value;
-  const trash = document.createElement("i");
-  trash.classList.add("fas", "fa-trash-alt");
-  const div  =  document.createElement("div");
-  div.classList.add('checktext')
+  const trash = document.createElement('i');
+  trash.classList.add('fas', 'fa-trash-alt');
+  const div = document.createElement('div');
+  div.classList.add('checktext');
   div.appendChild(checkbox);
   div.appendChild(para);
-  listItem.appendChild(div)
+  listItem.appendChild(div);
   listItem.appendChild(trash);
   list.appendChild(listItem);
-  
 };
 
-// map the todos from the arrays  
+// map the todos from the arrays
 
-const mapTasks = (tasks)=>{
-tasks.forEach(task=>{
-  addTodo(task.describtion);
-})
-}
+const mapTasks = (tasks) => {
+  tasks.forEach((task) => {
+    addTodo(task.describtion);
+  });
+};
 
-mapTasks(tasksArr)
-
-
+mapTasks(tasksArr);
